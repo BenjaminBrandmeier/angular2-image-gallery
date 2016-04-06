b217a6579436139ade8519f8b72f25013c7b75c4
Benjamin Brandmeier <ben.bra@gmx.de>
2016-04-06 19:49:45 +0000

    feat(routing): implemented dynamic base path + small changes to image viewer

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {GalleryApp} from './app/gallery';
+import {provide} from 'angular2/core';
+import {APP_BASE_HREF} from 'angular2/router';
 
-bootstrap(GalleryApp, []);
+bootstrap(GalleryApp, [provide(APP_BASE_HREF, {useValue : '/' })]);
