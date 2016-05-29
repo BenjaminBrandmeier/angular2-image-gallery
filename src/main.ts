9f9ef9c05b338a6dcd1537f9e57a6180e46e9d4c
Benjamin Brandmeier <ben.bra@gmx.de>
2016-05-29 18:27:10 +0000

    feat(mobile): more basic mobile support.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { enableProdMode } from '@angular/core';
 import { Router, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { GalleryAppComponent, environment } from './app/';
+import { APP_BASE_HREF } from '@angular/common';
 
 if (environment.production) {
   enableProdMode();
