aba60ca6c4861b3de0c925d157d6bba10cfb7fa6
Benjamin Brandmeier <ben.bra@gmx.de>
2017-01-07 15:45:14 +0100

    remove all generated js files

--- src/app/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var angular2imagegallery_module_1 = require('./angular2imagegallery.module');
-exports.Angular2ImageGalleryModule = angular2imagegallery_module_1.Angular2ImageGalleryModule;
-var gallery_component_1 = require('./gallery/gallery.component');
-exports.GalleryComponent = gallery_component_1.GalleryComponent;
-var image_service_1 = require('./services/image.service');
-exports.ImageService = image_service_1.ImageService;
-var viewer_component_1 = require('./viewer/viewer.component');
-exports.ViewerComponent = viewer_component_1.ViewerComponent;
