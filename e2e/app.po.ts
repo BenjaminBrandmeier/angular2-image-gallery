830d974f06219c49c032ff0ab4db4d44aff5b50a
Benjamin Brandmeier <ben.bra@gmx.de>
2017-04-21 20:44:06 +0200

    add six protractor tests for e2e testing. #29

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,27 @@ export class GalleryPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getFirstGalleryRow() {
+    return element(by.css('gallery > .galleryContainer > .innerGalleryContainer > .imagerow:nth-child(1)'));
+  }
+
+  getFirstImageFromFirstRow() {
+    return element(by.css('gallery > .galleryContainer > .innerGalleryContainer > .imagerow:nth-child(1) > img:nth-child(1)'));
+  }
+
+  getImageInsideViewerIfActive(id : number) {
+    return element(by.css('viewer > .outerContainer > .imageContainer > div.image.active:nth-child('+id+')'))
+  }
+
+  getExitButton() {
+    return element(by.css('viewer > .outerContainer > .buttonContainer > img.action.close'))
+  }
+
+  getLeftArrowButton() {
+    return element(by.css('viewer > .outerContainer > img.arrow.left'))
+  }
+
+  getRightArrowButton() {
+    return element(by.css('viewer > .outerContainer > img.arrow.right'))
   }
 }
